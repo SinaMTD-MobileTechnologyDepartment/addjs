8a092a0d34db2d7b44269f040941f6980d7a7c9a
Xiaojue <designsor@gmail.com>
2015-09-02 16:51:23 +0800

    add grunt and demo

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+var banner = [
+  '/**',
+  '<%=pkg.name%>',
+  '@author <%=pkg.author.name%> [<%=pkg.author.email%>]',
+  '@fileoverview <%=pkg.description%>',
+  '@vserion <%=pkg.version%>',
+  '**/',
+  ''
+].join('\r\n');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: banner
+      },
+      files: {
+        src: 'addJS.js',
+        dest: 'addJS.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', [
+    'uglify',
+  ]);
+
+};
